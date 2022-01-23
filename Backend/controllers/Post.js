1c816c8f57c0319548fa9c0a726ff1a0babbf3ea
SaladinX1 <hopefulhorizon01@gmail.com>
2022-01-23 16:49:02 +0100

    Etablissement du controller UpdatePost

--- Backend/controllers/Post.js
+++ Backend/controllers/Post.js
@@ -29,14 +29,29 @@ module.exports.createPost = (req, res) => {
         .then(res.status(201).json({
             message: 'Post crée !'
         }))
-
-
 }
 
 
 
 module.exports.updatePost = (req, res) => {
-
+    const ObjPost = req.file ? {
+        ...JSON.parse(req.body.post),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {
+        ...req.body
+    };
+    Post.updateOne({
+            id: req.params.id
+        }, {
+            ...ObjPost,
+            id: req.params.id
+        })
+        .then(() => res.status(200).json({
+            message: 'Post modifié !'
+        }))
+        .catch(error => res.status(400).json({
+            message: 'Muvaise requête !'
+        }));
 }
 
 module.exports.deletePost = (req, res) => {
