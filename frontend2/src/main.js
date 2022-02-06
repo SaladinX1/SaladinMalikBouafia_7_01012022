a9dabd2c05b5f4353ecd693c17205e41d3ea9026
SaladinX1 <hopefulhorizon01@gmail.com>
2022-02-06 17:30:58 +0100

    Instalation de Bootstrap

--- frontend2/src/main.js
+++ frontend2/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import VueRouter from 'vue-router'
 
 Vue.use(VueAxios, axios)
+
+Vue.use(bootstrap)
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
