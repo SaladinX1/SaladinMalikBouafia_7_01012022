dcccd26925f6de31bbfcf4f594b09ca4b3ec7b9e
SaladinX1 <hopefulhorizon01@gmail.com>
2022-02-10 18:21:36 +0100

    Intégration du path forum dans le dossier Routes.js

--- frontend2/src/Routes.js
+++ frontend2/src/Routes.js
@@ -2,17 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
+import Forum from './views/Forum'
 
 Vue.use(VueRouter)
 
 const routes = [{
-
   path: '/register',
   component: Register
 },
 {
   path: '/',
   component: Login
+},
+{
+  path: '/forum',
+  component: Forum
 }
 ]
 const router = new VueRouter({
