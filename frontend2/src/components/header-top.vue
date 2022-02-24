b0f2630bfa6db7b09f462bbda257f58f85c89559
SaladinX1 <hopefulhorizon01@gmail.com>
2022-02-24 11:02:56 +0100

    Etablissement du composant header-top

---
+++ frontend2/src/components/header-top.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+   <h1> Groupomonia </h1>
+</header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 70px;
+  position: fixed;
+  top: 0px;
+  border-bottom: 2px solid rgb(165, 46, 3);
+  background-color: rgb(209, 216, 218);
+}
+
+h1 {
+  display: inline;
+  color: rgb(0, 0, 0);
+  font-weight: 600;
+  margin: 0;
+  line-height: 1.5;
+}
+</style>
