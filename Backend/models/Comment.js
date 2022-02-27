cb5889b6509ef161b535a77e56ed9e139baca314
SaladinX1 <hopefulhorizon01@gmail.com>
2022-02-27 12:22:08 +0100

    Modification du modele comments avec implementation des relation many to many

--- Backend/models/Comment.js
+++ Backend/models/Comment.js
@@ -1,29 +1,30 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../Database/db.config');
+const User = require('../models/User')
+const Posts = require('../models/Post');
 
 
-const Comments = sequelize.define(`Comments`, {
+const Comments = sequelize.define('Comments', {
 
-    Id: {
-        type: Sequelize.NUMBER,
+    id: {
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
     },
-    Pseudo: {
-        type: Sequelize.STRING,
-        defaultValue: true,
-        length: 500
-    },
     message: {
         type: Sequelize.STRING,
-        defaultValue: true
-
-    },
-    timestamp: {
-        type: Sequelize.NUMBER
+        defaultValue: true,
+        length: 500,
     }
 
-})
+});
+
+User.hasMany(Comments);
+Comments.belongsTo(User);
+
+Posts.hasMany(Comments);
+Comments.belongsTo(Posts);
+
 
 module.exports = Comments;
