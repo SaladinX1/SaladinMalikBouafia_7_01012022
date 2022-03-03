c088372230515bb2e11c68bb8cf661d4dbeb76bc
SaladinX1 <hopefulhorizon01@gmail.com>
2022-03-03 23:44:00 +0100

    Ajout des boutons de connexion, inscription et deconnexion ainsi que leur design pour le composant header-top

--- frontend2/src/components/header-top.vue
+++ frontend2/src/components/header-top.vue
@@ -1,37 +1,108 @@
 <template>
     <header>
-   <h1> Groupomonia </h1>
+  <svg data-v-1084b650="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><!----> <!----> <rect data-v-1084b650="" fill="#FFFFFF" x="0" y="0" width="300px" height="300px" class="logo-background-square"></rect> <!----> <!----> <g data-v-1084b650="" id="640fb903-7c97-3a8b-d0a7-369102908d0a" fill="#FFD7D7" stroke="none" transform="matrix(0.8499999999999999,0,0,0.8499999999999999,22.500006484985377,22.50005836486818)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-205 207 100 100"><switch><g><path d="M-155 298.8c11.2 0 21.7-4.3 29.6-12.2 7.9-7.9 12.2-18.4 12.2-29.6 0-11.2-4.3-21.7-12.2-29.6-7.9-7.9-18.4-12.2-29.6-12.2s-21.7 4.3-29.6 12.2c-7.9 7.9-12.2 18.4-12.2 29.6 0 11.2 4.3 21.7 12.2 29.6 7.9 7.9 18.4 12.2 29.6 12.2zm2.4-7.1c-.8.1-1.6.1-2.4.1-1.1 0-2.2-.1-3.3-.2-3.6-5.6-6.3-11.6-7.9-17.9h17.7c.6 1.5 1.7 2.9 3 3.8-1.7 5-4.1 9.7-7.1 14.2zm9.5-2c1.9-3.5 3.4-7 4.6-10.7 2.9-.6 5.2-2.6 6.3-5.3h7.7c-4 7.3-10.7 13-18.6 16zm22.8-32.7c0 3.3-.5 6.5-1.3 9.6h-10.8c-.6-1.3-1.6-2.5-2.8-3.4.2-2.2.3-4.4.3-6.6 0-3.1-.2-6.2-.6-9.2h13.8c1 3 1.4 6.3 1.4 9.6zm-4.2-16.7h-12.2c-1.3-5.5-3.2-10.7-5.8-15.7 7.6 2.9 14 8.6 18 15.7zm-17.4 16.3c0 1.7-.1 3.4-.2 5.1-2.8.6-5.1 2.4-6.2 4.9h-19.3c-.4-3-.7-6.1-.7-9.1 0-1.9.1-3.8.3-5.7 2.6-.5 4.7-2.2 6-4.4h19.5c.4 3 .6 6.1.6 9.2zm-15.7-34.2c.9-.1 1.8-.1 2.6-.1 1 0 2 0 3 .1 3.6 5.6 6.3 11.6 7.9 17.9h-17.4c-.6-1.7-1.7-3.2-3.1-4.3 1.8-4.8 4.1-9.3 7-13.6zm-9.5 2c-1.7 3.2-3.2 6.6-4.4 10-3.1.6-5.6 2.9-6.6 5.8h-7.3c3.9-7.1 10.4-12.8 18.3-15.8zm-22.6 32.6c0-3.3.5-6.6 1.4-9.6h10.9c.6 1.1 1.5 2.1 2.5 2.9-.2 2.4-.4 4.8-.4 7.2 0 3.1.2 6.1.6 9.1h-13.6c-1-3.1-1.4-6.3-1.4-9.6zm16.2 16.6c1.3 5.4 3.2 10.7 5.8 15.7-7.6-3-13.9-8.8-15.7h12z"></path></g></switch></svg></g></svg> <h1 class="main-title"> Groupomania </h1>
+  <div class="log-status">
+    <button @click="logout()" class="logout">Deconnexion</button>
+    <button class="login" @click="directLogin()">Connexion</button>
+    <button class="register"  @click="directRegister()">inscription</button>
+  </div>
 </header>
 </template>
 
 <script>
+
+import logoutServices from '../services/logout'
+
 export default {
   name: 'header-top',
   data () {
     return {}
+  },
+  mounted () {
+    const hasTokenUser = localStorage.getItem('token')
+    const hasUserId = localStorage.getItem('userId')
+    if (hasTokenUser && hasUserId) {
+      const login = document.querySelector('.login')
+      const register = document.querySelector('.register')
+      login.style.display = 'none'
+      register.style.display = 'none'
+    } else {
+      const logout = document.querySelector('.logout')
+      logout.style.display = 'none'
+      const login = document.querySelector('.login')
+      const register = document.querySelector('.register')
+      login.style.display = 'block'
+      register.style.display = 'block'
+    }
+  },
+  methods: {
+    directRegister () {
+      this.$router.push({ path: '/register' })
+    },
+    directLogin () {
+      this.$router.push({ path: '/' })
+    },
+    logout () {
+      const token = localStorage.getItem('token')
+      logoutServices.logout(token).then(res => {
+        console.log(res)
+        document.cookie = `${res.data.token}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+        localStorage.removeItem('token')
+        sessionStorage.removeItem('token')
+        this.$router.push({ path: '/forum' })
+      }).catch(error => console.log(error))
+    }
   }
 }
 </script>
 
 <style scoped>
+
 header {
   display: flex;
   justify-content: center;
   width: 100%;
-  height: 80px;
+  height: 90px;
   position: fixed;
   top: 0px;
   border-bottom: 2px solid rgb(165, 46, 3);
   background-color: rgb(209, 216, 218);
 }
 
-h1 {
-    display: flex;
-    align-content: center;
-  display: inline;
+.main-title {
   color: rgb(0, 0, 0);
   font-weight: 600;
   margin: 0;
-  line-height: 1.5;
+  line-height: 2.1;
 }
+
+.log-status {
+display:flex;
+margin-left: 40%;
+}
+
+.login, .register {
+ outline: none;
+ cursor: pointer;
+ color: rgb(0, 0, 0);
+ background-color: rgb(122, 250, 110);
+ width: 50%;
+ margin: 2px 7px;
+ padding: 5px 10px;
+ border-radius: 8px;
+ padding: 5px 10px;
+}
+
+.logout {
+  background-color: rgb(255, 0, 0);
+  color: white;
+  outline: none;
+ cursor: pointer;
+ width: 90%;
+ margin: 2px 7px;
+ padding: 5px 10px;
+ border-radius: 8px;
+ padding: 5px 10px;
+}
+
 </style>
