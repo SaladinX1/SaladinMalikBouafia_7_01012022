e080e50347f998ffdf57975db37719ce9805f06c
SaladinX1 <hopefulhorizon01@gmail.com>
2022-03-05 14:04:33 +0100

    Ajout du middleware d'authentification dans  les routes posts et comments

--- Backend/routes/Comments.js
+++ Backend/routes/Comments.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const commentCtrl = require('../controllers/Comments');
 const auth = require('../middlewares/auth');
 
-router.delete('/:id',auth ,commentCtrl.deleteComment);
+router.delete('/:id',auth, commentCtrl.deleteComment);
 
 module.exports = router
--- Backend/routes/Post.js
+++ Backend/routes/Post.js
@@ -6,16 +6,16 @@ const auth = require('../middlewares/auth');
 
 //routes pour le CRUD des posts
 
-router.get('/', postCtrl.getAllPosts);
-router.get('/:id', postCtrl.getOnePost);
-router.post('/' ,auth,multer, postCtrl.createPost);
+router.get('/', auth,postCtrl.getAllPosts);
+router.get('/:id',auth, postCtrl.getOnePost);
+router.post('/' ,auth, multer, postCtrl.createPost);
 router.put('/:id',auth, multer, postCtrl.updatePost);
 router.delete('/:id',auth, postCtrl.deletePost);
-router.post('/:id/like-unlike-post', postCtrl.likeUnlikePost);
+router.post('/:id/like-unlike-post', postCtrl.likeDislikePost);
 
 //routes pour les commentaires
 
-router.get('/:id/comments', commentCtrl.getComments);
-router.post('/:id/comments', commentCtrl.createComment);
+router.get('/:id/comments',auth, commentCtrl.getComments);
+router.post('/:id/comments',auth, commentCtrl.createComment);
 
 module.exports = router;
