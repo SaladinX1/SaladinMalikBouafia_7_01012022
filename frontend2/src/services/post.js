edca37dd7269c160d51c7ef6163d22d09344b9d1
SaladinX1 <hopefulhorizon01@gmail.com>
2022-03-10 15:49:06 +0100

    Supréssion du paramètre userId dans les requêtes post

--- frontend2/src/services/post.js
+++ frontend2/src/services/post.js
@@ -5,13 +5,13 @@ const pathUrl = '/posts'
 
 const getAllPost = () => http.get(pathUrl)
 
-const addPost = (data, userId) => http.post(pathUrl, data, userId)
+const addPost = (data) => http.post(pathUrl, data)
 
 const getPostById = (id) => http.get(`${pathUrl}/${id}`)
 
 const deletePost = (id) => http.delete(`${pathUrl}/${id}`)
 
-const putPost = (id) => http.put(`${pathUrl}/${id}`)
+const putPost = (id, data) => http.put(`${pathUrl}/${id}`, data)
 
 export default {
   getAllPost,
