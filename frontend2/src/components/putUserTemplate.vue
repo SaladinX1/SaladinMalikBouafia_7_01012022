6cdb6031d8a0c7703969706fa939c73f50c58f69
SaladinX1 <hopefulhorizon01@gmail.com>
2022-03-11 13:06:48 +0100

    Etablissement de la view newPasswordView

--- frontend2/src/components/putUserTemplate.vue
+++ frontend2/src/components/putUserTemplate.vue
@@ -1,22 +1,19 @@
 <template>
     <div class="put-div" v-if='revealUser == true'>
              <h1>Modifie ton compte ici :</h1>
-        <form method="form">
+        <form method="form" @change="checkImputForm()">
             <div class="display-form-data">
               <label for="email">Email :</label>
               <p id="emailErrorMsg"></p>
-        <input type="text" name="email" v-model="email" >
+        <input class="input-pseudo-mail" type="text" name="email" pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-].+$" v-model="email">
               <label for="pseudo">Pseudo :</label>
-        <input type="text" name="pseudo" v-model="pseudo" >
+        <input class="input-pseudo-mail" type="text" name="pseudo" pattern="^[a-zA-Z]+[^0-9]" v-model="pseudo">
         <p id="pseudoErrorMsg"></p>
-              <label for="old-password"> Ancien mot de passe :</label>
-        <input type="text" name="old-password" v-model="old_password" required >
-              <label for="new-password">Nouveau mot de passe :</label>
-        <input type="text" name="new-password" v-model="new_password" required>
+              <button class="passwordButton" @click="newPasswordView()">Changer le mot de passe</button>
         </div>
         <div class="put-button-displaying">
         <button class="put-button-validation" @click="putUser()">Valider</button>
-        <button class="canceled" @click='togglePutUser()'>Annuler</button>
+        <button class="canceled" @click='togglePutUser' >Annuler</button>
         </div>
         </form>
     </div>
@@ -34,33 +31,30 @@ export default {
     }
   },
   mounted () {
-    const pseudo = document.querySelector('#pseudo')
-    pseudo.setAttribute('pattern', '^[a-zA-Z]+[^0-9]')
-    document.querySelector('#pseudo').addEventListener('change', (e) => {
-      const pseudo = e.target.value
-      if (/^[a-zA-Z]+[^0-9]/.test(pseudo) === false) {
-        document.querySelector('#pseudoErrorMsg').textContent = 'Veuillez sélectionnez un pseudo seulement par des lettres minuscules ou majuscules et/ou des chiffres'
-        const error = document.querySelector('#pseudo')
-        error.classList.add('border')
-        error.style.border = ' 1px solid red'
-      }
-    })
-    const email = document.querySelector('#email')
-    email.setAttribute('pattern', '^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-].+$')
-    document.querySelector('#email').addEventListener('change', (e) => {
-      const email = e.target.value
-      if (/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-].+$/.test(email) === false) {
-        document.querySelector('#emailErrorMsg').textContent = 'Veuillez inscrire une adresse éléctronique seulement avec un @ et des caractères alphanumériques et/ou spéciaux ( - , _ , .)'
-        const error = document.querySelector('#email')
-        error.classList.add('border')
-        error.style.border = ' 1px solid red'
-      }
-    })
   },
   methods: {
+    checkImputForm () {
+      document.querySelector('#pseudo').addEventListener('change', (e) => {
+        const pseudo = e.target.value
+        if (/^[a-zA-Z]+[^0-9]/.test(pseudo) === false) {
+          document.querySelector('#pseudoErrorMsg').textContent = 'Veuillez sélectionnez un pseudo seulement par des lettres minuscules ou majuscules et/ou des chiffres'
+          const error = document.querySelector('#pseudo')
+          error.classList.add('border')
+          error.style.border = ' 1px solid red'
+        }
+      })
+      document.querySelector('#email').addEventListener('change', (e) => {
+        const email = e.target.value
+        if (/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-].+$/.test(email) === false) {
+          document.querySelector('#emailErrorMsg').textContent = 'Veuillez inscrire une adresse éléctronique seulement avec un @ et des caractères alphanumériques et/ou spéciaux ( - , _ , .)'
+          const error = document.querySelector('#email')
+          error.classList.add('border')
+          error.style.border = ' 1px solid red'
+        }
+      })
+    },
     putUser () {
-      const userId = localStorage.getItem('userId')
-      userService.putUser(this.pseudo, userId).then(post => {
+      userService.putUser(this.pseudo).then(post => {
         console.log(post)
         alert('Compte modifié ! bravo ! 😃')
         this.$router.push({ path: '/forum' })
@@ -70,11 +64,14 @@ export default {
     updatePicture (event) {
       this.picture = event.target.files[0]
     },
+    newPasswordView () {
+      this.$router.push({ path: '/new_password' })
+    },
     backToForum () {
       this.$router.push({ path: '/forum' })
     }
   },
-  props: ['revealUser']
+  props: ['revealUser', 'togglePutUser']
 }
 </script>
 
@@ -102,11 +99,17 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
+    line-height: 2.2;
 }
 
 label {
   color: white;
   font-size: 1.6rem;
+}
+
+.input-pseudo-mail {
+  font-size: 1.3rem;
+  font-weight: 500;
 }
 
 .area-put {
@@ -118,15 +121,24 @@ display: flex;
 justify-content: flex-start;
 }
 
+.passwordButton {
+  width: 50%;
+  border-radius: 5px;
+  outline: none;
+  font-size: 1.1rem;
+}
+
 .put-button-validation {
     margin: 10px;
     background-color : greenyellow;
+    font-size: 1.1rem;
 }
 
 .canceled {
     margin: 10px;
     background-color : rgb(255, 47, 47);
     color: white;
+    font-size: 1.1rem;
 }
 
 </style>
