8304e3f748a39d46e02f2bda5c14424e95e26f1d
SaladinX1 <hopefulhorizon01@gmail.com>
2022-03-13 17:59:47 +0100

    Suppression de la fonctionalité putPass dans le dossier service user du front

--- frontend2/src/services/user.js
+++ frontend2/src/services/user.js
@@ -6,13 +6,10 @@ const deleteUser = () => http.delete(`${pathUrl}/delete_user`)
 
 const putUser = (putUser) => http.put(`${pathUrl}/put_user`, putUser)
 
-const putPass = (putPassword) => http.put(`${pathUrl}/put_user`, putPassword)
-
 const getUser = () => http.get(`${pathUrl}/get_user`)
 
 export default {
   deleteUser,
   putUser,
-  putPass,
   getUser
 }
